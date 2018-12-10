d6d19d5776a5ec9609debfb4802726ce56e9100a
lidefu <18354236885@qq.com>
2018-12-10 10:36:11 +0800

    行云分页测试

--- src/main/java/com/ldf/pagehelper/App.java
+++ src/main/java/com/ldf/pagehelper/App.java
@@ -21,10 +21,19 @@ public class App {
     @Bean
     public DataSource myDataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        //三峡
+//        dataSource.setDriverClassName("com.bonc.xcloud.jdbc.XCloudDriver");
+//        dataSource.setUrl("jdbc:xcloud:@172.16.3.46:1803/bonc_sxxny_ly?connectRetry=3&socketTimeOut=60000000&connectDirect=false");
+//        dataSource.setUsername( "sxxny" );
+//        dataSource.setPassword( "sxxny1q2w3e" );
+
+        //天柱山
         dataSource.setDriverClassName("com.bonc.xcloud.jdbc.XCloudDriver");
-        dataSource.setUrl("jdbc:xcloud:@172.16.3.46:1803/bonc_sxxny_ly?connectRetry=3&socketTimeOut=60000000&connectDirect=false");
-        dataSource.setUsername( "sxxny" );
-        dataSource.setPassword( "sxxny1q2w3e" );
+        dataSource.setUrl("jdbc:xcloud:@172.16.3.46:1803/bonc_tzs_ly?connectRetry=3&socketTimeOut=60000000&connectDirect=false");
+        dataSource.setUsername( "tzs" );
+        dataSource.setPassword( "tzs1q2w3e" );
+
         return dataSource;
     }
 
